2d746f5b8fd894fb24ab4fdf903a50403dae03a8
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2024-10-03 14:12:52 +0530

    UPDATE

--- js/index.js
+++ js/index.js
@@ -1,10 +1,9 @@
-document.getElementById("left-arrow").onclick = function() {
-    document.getElementById("hero-first").style.display = "none";
-    document.getElementById("hero-second").style.display = "block";
-}
+    document.getElementById("left-arrow").onclick = function() {
+        document.getElementById("hero-first").style.display = "none";
+        document.getElementById("hero-second").style.display = "block";
+    }
+        
+    
 
-//KEHFLKQA
 
-//ASJVLAHS
 
-// AMSDCJKASDC
