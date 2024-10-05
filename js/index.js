6424b657a979bcd22149306dbfb60bae34e37c26
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2024-10-05 14:43:49 +0530

    UPDATE

--- js/index.js
+++ js/index.js
@@ -3,4 +3,16 @@
         document.getElementById("hero-second").style.display = "block";
     }
   
-    //sjdvkjslkvj
+    document.getElementById("bar-1").onclick = function(){
+        document.getElementById("show-mobile-tab").style.display = "block"; 
+    }
+
+    // document.getElementById("body").onclick = function(){
+    //     document.getElementById("show-mobile-tab").style.display = "none";
+    // }
+
+document.getElementById("mobile-page").onclick = function(){
+    document.getElementById("mobile-page-button").style.display = "block";
+    document.getElementById("page-arrow-id-2").style.display = "block";
+    document.getElementById("page-arrow-id").style.display = "none"; 
+}
