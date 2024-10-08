0202b5cdff11b71b221b16642f2444575f36c050
Gauravkumawat09 <gauravkumawatgk.09@gmail.com>
2024-10-08 23:25:28 +0530

    UPDATE

--- js/shop.js
+++ js/shop.js
@@ -53,8 +53,4 @@ document.getElementById("prize").onclick = function(){
       document.getElementById("sixth-category").style.display = "block";
   }
 
-  //fyufiyt
-
-  //uguifui
-
-  //jhgukgiuy
+  //hguygiyfy
